8166276ab75fec5aee6acc531349125657e46ad9
Judah Perez <tythin4@gmail.com>
2018-06-22 21:31:38 -0700

    animal input movement

--- Assets/Scripts/Animals/AnimalController.cs
+++ Assets/Scripts/Animals/AnimalController.cs
@@ -8,9 +8,21 @@ public class AnimalController : MonoBehaviour
     [SerializeField]
     private CharacterController _characterController;
 
+    [SerializeField]
     private Vector3 _movement;
 
-	// Use this for initialization
+    [SerializeField]
+    private float _moveSpeed = 5;
+
+    [SerializeField]
+    private int life = 10;
+
+    public bool Dead
+    {
+        get { return life < 1; }
+    }
+
+    // Use this for initialization
     void Start()
     {
         if (_characterController == null)
@@ -20,9 +32,19 @@ public class AnimalController : MonoBehaviour
     }
 	
 	// Update is called once per frame
-	void Update () {
-        
-    }
+	void Update ()
+	{
+        if(Dead)
+            return;
+
+	    _characterController.Move(_movement * _moveSpeed * Time.deltaTime);
+
+	    if (_movement.magnitude > 0)
+	    {
+	        var rot = Quaternion.LookRotation(_movement);
+	        _characterController.transform.rotation = Quaternion.Lerp(_characterController.transform.rotation, rot, 10 * Time.deltaTime);
+	    }
+	}
 
     public void Move(Vector3 movement)
     {

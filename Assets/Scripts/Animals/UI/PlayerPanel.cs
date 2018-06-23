7b14a6f864e20e2d448a700a20ee2a842be23371
Judah Perez <tythin4@gmail.com>
2018-06-23 12:06:43 -0700

    Simple score ui

---
+++ Assets/Scripts/Animals/UI/PlayerPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPanel : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text _nameText;
+    [SerializeField]
+    private Text _scoreText;
+
+    public void SetPlayer(string name)
+    {
+        _nameText.name = name;
+    }
+
+    public void SetScore(int score)
+    {
+        _scoreText.text = "Fruit: " + score;
+    }
+}
---
+++ Assets/Scripts/Animals/UI/PlayerPanels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPanels : MonoBehaviour
+{
+
+    [SerializeField] private PlayerPanel _panelPrefab;
+
+    [SerializeField] private RectTransform _parentPanel;
+
+    [SerializeField] private List<PlayerPanel> _panels = new List<PlayerPanel>();
+
+    [SerializeField] private List<AnimalController> _players = new List<AnimalController>();
+    // Use this for initialization
+    void Start () {
+        for (int cnt = 0; cnt < _players.Count; cnt++)
+        {
+            var panel = Instantiate(_panelPrefab, _parentPanel);
+            _panels.Add(panel);
+
+            panel.SetPlayer("Player " + (cnt+1));
+
+            var collect = _players[cnt].GetComponent<FruitCollect>();
+            collect.OnPointsGained.AddListener(panel.SetScore);
+
+        }
+	}
+	
+}

c5fc23669499a9d1ca1d3f102a47e540bb0b7cb0
Judah Perez <tythin4@gmail.com>
2018-06-22 22:29:42 -0700

    Banana fruit throw

---
+++ Assets/Scripts/Animals/Fruit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Fruit : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody _rigidbody;
+    [SerializeField]
+    private Vector3 _throwDir;
+
+	// Use this for initialization
+	void Start () {
+	    _rigidbody.AddForce(_throwDir, ForceMode.Impulse);
+
+    }
+	
+
+}

2cba2969603095171bd3563bc15751ac00ee8f8d
Judah Perez <tythin4@gmail.com>
2018-06-22 20:57:31 -0700

    Aminal controller does nothing

---
+++ Assets/Scripts/Animals/AnimalController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class AnimalController : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController _characterController;
+
+    private Vector3 _movement;
+
+	// Use this for initialization
+    void Start()
+    {
+        if (_characterController == null)
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void Move(Vector3 movement)
+    {
+        _movement = movement;
+        _movement.y = 0;
+    }
+}

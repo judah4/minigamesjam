a5cdb913fe66216e502321485505ee0afb1dff23
Judah Perez <tythin4@gmail.com>
2018-06-23 09:30:40 -0700

    Fruit Pickup

---
+++ Assets/Scripts/Animals/FruitCollect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class FruitEvent : UnityEvent<int>
+{
+
+}
+
+public class FruitCollect : MonoBehaviour
+{
+    [SerializeField]
+    private int _fruit = 0;
+
+    public int Fruit
+    {
+        get { return _fruit; }
+    }
+
+
+    public FruitEvent OnPointsGained;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collide " + collision.gameObject.name);
+
+        if (collision.transform.tag != "Fruit")
+            return;
+        
+        //collect
+        _fruit++;
+
+        Destroy(collision.gameObject);
+
+        if (OnPointsGained != null)
+        {
+            OnPointsGained.Invoke(_fruit);
+        }
+
+    }
+}

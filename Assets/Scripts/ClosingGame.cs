0e52cc3d9b3dc4bea9cad38a4c0d66df24cd52e2
Unknown <antoniocervantes1@live.com>
2018-06-23 20:18:00 -0700

    Code for quit game

---
+++ Assets/Scripts/ClosingGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(Keycode.Escape))
+            Application.Quit();
+
+    }
+}

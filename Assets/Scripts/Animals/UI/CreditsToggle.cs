6717f6c56870ade8142681793104914c1c42343f
Judah Perez <tythin4@gmail.com>
2018-06-24 17:56:03 -0700

    Credits

---
+++ Assets/Scripts/Animals/UI/CreditsToggle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsToggle : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _creditsPanel;
+
+	// Use this for initialization
+	void Start () {
+	    _creditsPanel.SetActive(false);
+
+    }
+
+    public void Toggle()
+    {
+        _creditsPanel.SetActive(!_creditsPanel.activeInHierarchy);
+
+    }
+
+
+}
